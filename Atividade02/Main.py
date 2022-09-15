41763a90cf5fb7e34cf66746e8e63d5d084000c5
Elias_Dias <eliasdiasdearaujo2@gmail.com>
2022-09-15 18:53:22 -0300

    Create: Inserindo cidades

--- Atividade02/Main.py
+++ Atividade02/Main.py
@@ -5,7 +5,38 @@ from Transiction import Transiction
 road_map = [
     Stat("Oradea", [Transiction([151, "Sibiu"]), Transiction([71, "Zerind"])]),
     Stat("Zerind", [Transiction([71, "Oradea"]), Transiction([75, "Arad"])]),
-    # Colocar as novas cidades aqui, seguindo o padrão acima  #
+    Stat("Arad", [Transiction([75, "Zerind"]), Transiction(
+        [140, "Sibiu"]), Transiction([118, "Timisoara"])]),
+    Stat("Timisoara", [Transiction([118, "Arad"]),
+         Transiction([111, "Lugoj"])]),
+    Stat("Lugoj", [Transiction([111, "Timisoara"]),
+         Transiction([70, "Mehadin"])]),
+    Stat("Mehadin", [Transiction([70, "Lugoj"]),
+         Transiction([75, "Drobeta"])]),
+    Stat("Drobeta", [Transiction([75, "Mehadin"]),
+         Transiction([120, "Craiova"])]),
+    Stat("Sibin", [Transiction([151, "Oraden"]), Transiction([140, "Arad"]), Transiction(
+        [99, "Fagaras"]), Transiction([80, "Rimnicu Vielcea"])]),
+    Stat("Rimnicu Vilcea", [Transiction([80, "Sibin"]), Transiction(
+        [97, "Pitesti"]), Transiction([146, "Craiova"])]),
+    Stat("Craiova", [Transiction([146, "Rimnicu Vilcea"]), Transiction(
+        [138, "Pitesti"]), Transiction([120, "Drobeta"])]),
+    Stat("Fagaras", [Transiction([99, "Sibiu"]),
+         Transiction([211, "Bucharest"])]),
+    Stat("Pitesti", [Transiction([97, "Rimnicu Vilcea"]), Transiction(
+        [101, "Bucharest"]), Transiction([138, "Craiova"])]),
+    Stat("Bucharest", [Transiction([211, "Fagaras"]), Transiction(
+        [101, "Pitesti"]), Transiction([90, "Giurgiu"]), Transiction([85, "Urziceni"])]),
+    Stat("Girgiu", [Transiction([90, "Bucharest"])]),
+    Stat("Urziceni", [Transiction([98, "Hirsova"]), Transiction(
+        [142, "Vaslui"]), Transiction([85, "Bucharest"])]),
+    Stat("Hirsova", [Transiction([98, "Urziceni"]),
+         Transiction([86, "Efroie"])]),
+    Stat("Vaslui", [Transiction([142, "Urziceni"]),
+         Transiction([92, "Iasi"])]),
+    Stat("Iasi", [Transiction([92, "Vaslui"]), Transiction([87, "Neamt"])]),
+    Stat("Eforie", [Transiction([86, "Hirsova"])]),
+    Stat("Neamt", [Transiction([87, "Iasi"])])
 ]
 
 for item in road_map:
--- Atividade02/Stat.py
+++ Atividade02/Stat.py
@@ -1,6 +1,3 @@
-from typing import List
-
-
 class Stat:
     def __init__(self, name, transictions):
         self.name = name
